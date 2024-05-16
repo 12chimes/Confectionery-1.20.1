3c87ce9636078e0ffa826f2cb55ec3fb516b9050
midnight <midnight1201@proton.me>
2024-05-16 18:38:04 +1000

    minor changes

--- src/main/java/uwu/cocoa/confectionery/item/ModFoods.java
+++ src/main/java/uwu/cocoa/confectionery/item/ModFoods.java
@@ -5,8 +5,8 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties CANDY_CANE = new FoodProperties.Builder().nutrition(2).fast()
-            .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.DIG_SPEED, 200), 0.1f).build();
+    public static final FoodProperties CANDY_CANE = new FoodProperties.Builder().nutrition(2).fast().alwaysEat()
+            .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.DIG_SPEED, 200), 1f).build();
 
 
     //add properties for items that are foods
